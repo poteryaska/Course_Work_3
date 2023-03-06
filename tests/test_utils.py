2678b23031e6bf8e66746c827dfde06ef4217e91
evgeniia lemanova <jane87.05@mail.ru>
2023-03-06 11:53:09 +0300

    all project

---
+++ tests/test_utils.py
@@ -0,0 +1,80 @@
+import pytest
+from utils import get_executed_operation, get_last_five_operations, get_total_information
+
+@pytest.fixture
+def test_data():
+    return [
+  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  },
+  {
+    "id": 41428829,
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  },
+  {
+    "id": 939719570,
+    "state": "CANCELED",
+    "date": "2018-06-30T02:08:58.425572",
+    "operationAmount": {
+      "amount": "9824.07",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 75106830613657916952",
+    "to": "Счет 11776614605963066702"
+  },
+  {
+    "id": 587085106,
+    "state": "EXECUTED",
+    "date": "2018-03-23T10:45:06.972075",
+    "operationAmount": {
+      "amount": "48223.05",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 41421565395219882431"
+  }
+]
+
+def test_get_executed_operation(test_data):
+    data = get_executed_operation(test_data)
+    assert len(data) == 2
+
+def test_get_last_five_operations(test_data):
+    data = get_last_five_operations(test_data, 2)
+    assert data[0]["date"] == "2019-08-26T10:50:58.294041"
+    assert len(data) == 2
+
+def test_get_total_information(test_data):
+    data = get_total_information(test_data[:1])
+    assert data == ['26.08.2019 Перевод организации\nMaestro 1596 83** **** 5199 -> Счет **9589\n31957.58 руб.\n']
+    data = get_total_information(test_data[-1:])
+    assert data == ['23.03.2018 Открытие вклада\nAноним  -> Счет **2431\n48223.05 руб.\n']
